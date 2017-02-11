35624f99caf964adedc0c949d55fee8eb3baa718
felixacdc <pfmata01@gmail.com>
2017-02-10 20:19:22 -0600

    [add] The-render-method

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -10,7 +10,13 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return <h1>Hello Series</h1>
+    //return <h1>Hello World</h1> <b>Bold</b> //Incorrecto
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <b>Bold</b>
+      </div>
+    )
   }
 }
 
