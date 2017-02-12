12fe7f4aa9eae2605959027f5dcdf98ce524aac5
felixacdc <pfmata01@gmail.com>
2017-02-12 10:18:53 -0600

    [add] React synthetic event system

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -75,24 +75,54 @@ import React from 'react';
 // }
 
 ////
+// class App extends React.Component {
+//   render() {
+//     return <Title text="The Text" />;
+//   }
+// }
 
+// const Title = (props) => <h1>Title: {props.text}</h1>;
+
+// Title.propTypes = {
+//     // text: React.PropTypes.string.isRequired
+//     text(props, propName, component) {
+//         if(!(propName in props))
+//             return new Error(`Missing ${propName}`)
+
+//         if(props[propName].length < 6)
+//             return new Error(`${propName} was too short`)
+//     }
+// }
+////
 class App extends React.Component {
-  render() {
-    return <Title text="The Text" />;
+  constructor() {
+    super();
+    this.state = {curretEvent: '---'}
+    this.update = this.update.bind(this)
   }
-}
-
-const Title = (props) => <h1>Title: {props.text}</h1>;
-
-Title.propTypes = {
-    // text: React.PropTypes.string.isRequired
-    text(props, propName, component) {
-        if(!(propName in props))
-            return new Error(`Missing ${propName}`)
-
-        if(props[propName].length < 6)
-            return new Error(`${propName} was too short`)
-    }
+  update(e) {
+  this.setState({currentEvent: e.type})
+  }
+  render() {
+    return (
+        <div>
+          <textarea
+            onKeyPress={this.update}
+            onCopy={this.update}
+            onCut={this.update}
+            onPaste={this.update}
+            onFocus={this.update}
+            onBlur={this.update}
+            onDoubleClick={this.update}
+            onTouchStart={this.update}
+            onTouchMove={this.update}
+            onTouchEnd={this.update}
+            cols="30" 
+            rows="10"/>
+          <h1>{this.state.currentEvent}</h1>
+        </div>
+    )
+  }
 }
 
 export default App;
