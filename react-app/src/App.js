2838bb58a94a9aec626854de3f793dfeaaf80d45
felixacdc <pfmata01@gmail.com>
2017-02-12 21:52:16 -0600

    [modify] Component lifecycle mounting basics

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -162,6 +162,8 @@ class Input extends React.Component {
   }
 }*/
 
+import ReactDOM from 'react-dom';
+
 class App extends React.Component {
   constructor(){
     super();
@@ -181,6 +183,27 @@ class App extends React.Component {
   componentDidMount() {
     console.log('componentDidMount')
   }
+  componentWillUnmount() {
+    console.log('componentWillUnmount')
+  }
 }
 
-export default App;
+class Wrapper extends React.Component {
+  mount() {
+    ReactDOM.render(<App />, document.getElementById('a'))
+  }
+  unmount() {
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.mount.bind(this)}>Mount</button>
+        <button onClick={this.unmount.bind(this)}>UnMount</button>
+        <div id="a"></div>
+      </div>
+    )
+  }
+}
+
+export default Wrapper;
