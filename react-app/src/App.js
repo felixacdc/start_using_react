287daf75a3054b84bace13d98ac447de953e4206
felixacdc <pfmata01@gmail.com>
2017-02-14 16:24:35 -0600

    [add] React fundamentals higher order components replaces mixins

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -289,7 +289,7 @@ class App extends React.Component {
 
 App.defaultProps = {val: 0}*/
 
-class App extends React.Component {
+/*class App extends React.Component {
   constructor() {
     super();
   
@@ -321,5 +321,39 @@ class App extends React.Component {
 }
 
 const Person = (props) => <h4>{props.person.name}</h4>
+
+export default App;*/
+
+const HOC = (InnerComponent) => class extends React.Component {
+  render() {
+    return <InnerComponent
+      {...this.props}
+    />
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Button>button</Button>
+        <hr/>
+        <LabelHOC>label</LabelHOC>
+      </div>
+    )
+  }
+}
+
+const Button = HOC((props) => <button>{props.children}</button>)
+
+class Label extends React.Component {
+  render() {
+    return (
+      <label>{this.props.children}</label>
+    )
+  }
+}
+
+const LabelHOC = HOC(Label)
 
 export default App;
