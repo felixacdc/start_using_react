561e12c753d91e69303015db8036116dee4cad69
felixacdc <pfmata01@gmail.com>
2017-02-14 16:38:57 -0600

    [modify] React fundamentals higher order components replaces mixins

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -325,9 +325,22 @@ const Person = (props) => <h4>{props.person.name}</h4>
 export default App;*/
 
 const HOC = (InnerComponent) => class extends React.Component {
+  constructor() {
+    super();
+  
+    this.state = {count: 0};
+  }
+  update() {
+    this.setState({count: this.state.count + 1})
+  }
+  componentWillMount() {
+    console.log('Will mount')
+  }
   render() {
     return <InnerComponent
       {...this.props}
+      {...this.state}
+      update={this.update.bind(this)}
     />
   }
 }
@@ -344,12 +357,15 @@ class App extends React.Component {
   }
 }
 
-const Button = HOC((props) => <button>{props.children}</button>)
+const Button = HOC((props) => <button onClick={props.update}>{props.children} - {props.count}</button>)
 
 class Label extends React.Component {
+  componentWillMount() {
+    console.log('Label will mount')
+  }
   render() {
     return (
-      <label>{this.props.children}</label>
+      <label onMouseMove={this.props.update}>{this.props.children} - {this.props.count}</label>
     )
   }
 }
