56abf6308337e356393d190d83306e3d1582ed99
felixacdc <pfmata01@gmail.com>
2017-02-17 21:49:26 -0600

    [add] JSX live compiler

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -324,7 +324,7 @@ const Person = (props) => <h4>{props.person.name}</h4>
 
 export default App;*/
 
-const HOC = (InnerComponent) => class extends React.Component {
+/*const HOC = (InnerComponent) => class extends React.Component {
   constructor() {
     super();
   
@@ -370,6 +370,47 @@ class Label extends React.Component {
   }
 }
 
-const LabelHOC = HOC(Label)
+const LabelHOC = HOC(Label)*/
+
+class App extends React.Component {
+  constructor() {
+    super();
+  
+    this.state = {
+      input: '/* add your jsx here */',
+      output: '',
+      err: ''
+    };
+  }
+  update(e) {
+    let code = e.target.value;
+    try {
+      this.setState({
+        output: window.Babel
+        .transform(code, { presets: ['es2015', 'react']})
+        .code,
+        err: ''
+      })
+    } catch(err) {
+      this.setState({
+        err: err.message
+      })
+    }
+  }
+  render() {
+    return (
+      <div>
+        <header>{this.state.err}</header>
+        <div className="container">
+          <textarea onChange={this.update.bind(this)}
+          defaultValue={this.state.input}/>
+          <pre>
+            {this.state.output}
+          </pre>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default App;
